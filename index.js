ea0da8de69166a1bc01f29877b11407245b6558b
MuslumYilmaz <muslum@vizyon.ai>
2019-05-16 10:50:24 +0300

    show answers func

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const correctAnswers = ['B', 'B', 'B', 'B'];
+const correctAnswers = ['B', 'A', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
 const result = document.querySelector('.result');
+const answerCheck = document.querySelector('.show');
+const trueAnswers = document.getElementsByClassName('answers');
+
+
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     let score = 0;
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
-
+   
     //check answers
     userAnswers.forEach((answer, index) => {
         if (answer === correctAnswers[index]){
@@ -34,3 +38,11 @@ form.addEventListener('submit', e => {
 
 });
 
+answerCheck.addEventListener('click', e => {
+    e.preventDefault();
+    for (let i = 0; i < trueAnswers.length; i++) {
+        trueAnswers[i];
+        trueAnswers[i].style.border = '2px dotted #0d47a1';
+        trueAnswers[i].style.borderRadius = '50px';
+}
+});
